7524daad2d40fd7a0ba3a19b73b30666078aef08
neo <matheusventureli50@gmail.com>
2023-09-30 20:15:40 -0300

    Imagem docker reduzida em 141MB

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM openjdk:21
+FROM openjdk:18-alpine
 
 WORKDIR /app
 
-COPY target/resistance-social-network-0.0.1-SNAPSHOT.jar /app/resistance-social-network.jar
+COPY target/resistance-social-network-0.0.1-SNAPSHOT.jar /app/app.jar
 
 EXPOSE 8080
 
-ENTRYPOINT ["java", "-jar", "resistance-social-network.jar"]
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/api/resistancesocialnetwork/ResistanceSocialNetworkApplication.java
+++ src/main/java/com/api/resistancesocialnetwork/ResistanceSocialNetworkApplication.java
@@ -2,16 +2,8 @@ package com.api.resistancesocialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "com.api.resistancesocialnetwork.model"
-})
-@EnableJpaRepositories(basePackages = {
-        "com.api.resistancesocialnetwork.repositories"
-})
 public class ResistanceSocialNetworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResistanceSocialNetworkApplication.class, args);
