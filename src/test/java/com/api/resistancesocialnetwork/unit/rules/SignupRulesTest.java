fdfb88e01a010efabbb685466720aff19fcda71f
neonabuko <matheusventureli50@gmail.com>
2024-04-16 16:55:21 -0300

    remove list warning

--- src/test/java/com/api/resistancesocialnetwork/unit/rules/SignupRulesTest.java
+++ src/test/java/com/api/resistancesocialnetwork/unit/rules/SignupRulesTest.java
@@ -7,7 +7,8 @@ import com.api.resistancesocialnetwork.rules.commons.ResistanceException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SignupRulesTest {
     private final SignupRules signupRules = new SignupRules();
